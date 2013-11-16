d0e267940fb521224e24b94dd7950d0a2f1227fd
Sean Anderson <github@seananderson.ca>
2013-11-15 16:55:46 -0800

    Update get_wasp_trips to work with dplyr

--- R/get_wasp_trips.r
+++ R/get_wasp_trips.r
@@ -11,10 +11,11 @@
 #' year 1980 will be returned. This behaviour is required to comply
 #' with the \pkg{data.table} package.
 #'
-#' @param uid A vector of uid values.
-#' @param address A vector of numeric addresses.
-#' @param time A vector of times in character format. Should match the
-#' format specified in \code{time_format}.
+#' @param x A data.frame containing columns named \code{uid},
+#' \code{address}, and \code{time}. The \code{time} should be in
+#' character format and match the format declared to the argument
+#' \code{time_format}. \code{uid} should be of type character and
+#' \code{address} of type integer.
 #' @param time_format Time format to be passed to
 #' \code{strptime}.
 #' @param diff_address_home First difference value of \code{address}
@@ -24,17 +25,15 @@
 #' @param print_progress Should the uid be printed? Useful to monitor
 #' progress when running this function over large datasets.
 #' @param correct_time_stamps Should the time stamps be corrected. The
-#' values are hardcoded in the function.
+#' values are currently hard-coded into the function.
 #' @return
-#' A data.frame containing columns of time, trip type, and time away
-#' or home.
+#' A data.frame containing columns of time, uid, trip type (away or
+#' home), and time away or home.
 #' @export
 
-get_wasp_trips <- function(uid, address, time, time_format =
-  "%m/%d/%Y %H:%M:%S", diff_address_home = 2, diff_address_away = -2,
+get_wasp_trips <- function(x, time_format = "%m/%d/%Y %H:%M:%S",
+  diff_address_home = 2, diff_address_away = -2,
   print_progress = TRUE, correct_time_stamps = TRUE) {
-  # Make a data.frame with the arguments:
-  x <- data.frame(uid, address, time)
   # Print the uid to keep track of progress:
   if(print_progress) message(as.character(x$uid[1]))
   # Take a first difference on the address column:
@@ -51,28 +50,18 @@ get_wasp_trips <- function(uid, address, time, time_format =
   # Remove these columns to save memory:
   x$diff_address <- NULL
   x$address <- NULL
-  x$uid <- NULL
-  # And make a dummy time object that we'll use below
-  dummy_time <- strptime("01/01/1980 14:47:10", format = time_format)
   # If there are any rows to work with, take the first difference of the
   # times. This gives us time between trips:
   if (nrow(x) > 0) {
+    x$time <- strptime(x$time, format = time_format)
     if(correct_time_stamps) {
       # Now correct the time stamps:
       actual_time <- strptime("11/05/2013 14:47:10", format = time_format)
       preset_time <- strptime("02/11/2005 05:50:34", format = time_format)
       x$time <- x$time + (actual_time - preset_time)
     }
-      x$time <- strptime(x$time, format = time_format)
-
     x$time_diff <- as.numeric(c(NA, diff(x$time)))
     out <- x
-  } else {
-    # data.table needs a matching data.frame; we'll delete these rows after
-    out <- data.frame(time = dummy_time, trips = "IGNORE", time_diff =
-      999, stringsAsFactors = FALSE)
-    #out$time_diff <- as.numeric(out$time_diff)
+    return(out)
   }
-  # And return the data frame for this chunk:
-  return(out)
 }
